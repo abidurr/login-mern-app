07f304d4a9a0ab83b82934f5aa7ba672f4ff0481
abidurr <42abid@gmail.com>
2020-04-26 01:23:38 -0400

    Add files

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    mongoURI: "mongodb+srv://admin:iKLF17JTfEQrvOa1@cluster0-6oqrf.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true",
+    
+    secretOrKey: "secret" 
+};
+
